071701e1ca608a3fb6d1555778b29fd42941d6ee
saocode <saospam@gmail.com>
2020-10-19 13:31:26 -0700

    Error

---
+++ htdocs/error.php
@@ -0,0 +1,26 @@
+<style>
+.blink_me {
+  animation: blinker 1.5s linear infinite;
+  font-weight:bolder;
+  color:red;
+}
+@keyframes blinker {
+  50% {
+    opacity: 0;
+  }
+</style>
+
+<div class="blink_me" style="position: absolute; top: 10%; left: 10%;}">
+<pre>                                                
+███████╗██████╗ ██████╗  ██████╗ ██████╗ 
+██╔════╝██╔══██╗██╔══██╗██╔═══██╗██╔══██╗
+█████╗  ██████╔╝██████╔╝██║   ██║██████╔╝
+██╔══╝  ██╔══██╗██╔══██╗██║   ██║██╔══██╗
+███████╗██║  ██║██║  ██║╚██████╔╝██║  ██║
+╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝
+                                         
+
+</pre>                
+</div>      
+
+                          
