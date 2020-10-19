931c589d438fc44575f8489a5fe1a75de03d61fa
saocode <saospam@gmail.com>
2020-10-19 14:24:34 -0700

    Update header.php

--- htdocs/bcit/header.php
+++ htdocs/bcit/header.php
@@ -56,6 +56,8 @@ if (is_dir($directory)) {
         closedir($handle);
     }
 }
+echo "<a href=\"$appURL/bcit-phpmyadmin\" target=\"_blank\" class=\"w3-bar-item w3-button\">PHP My Admin</a>\r\n";
+
 echo "<a href=\"$appURL/dvwa\" target=\"_blank\" class=\"w3-bar-item w3-button\">Damn Vulnerable Web App</a>\r\n";
 // Output each file as a hyperlink
 foreach ($menuArray as $value) {
